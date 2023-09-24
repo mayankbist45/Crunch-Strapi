b4f06f04ff04447757121ef9552018ad616d8cd4
Mayank Bist <mayankb@zeta.tech>
2023-09-24 17:15:35 +0530

    Add testimonial data

---
+++ src/api/testimonial-component/controllers/testimonial-component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * testimonial-component controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::testimonial-component.testimonial-component');
---
+++ src/api/testimonial-component/services/testimonial-component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * testimonial-component service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::testimonial-component.testimonial-component');
